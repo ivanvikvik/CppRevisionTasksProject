a60ca1bbefe3be6aea0d6caab5213ddcabd9c3bd
= <=>
2022-05-19 15:25:52 +0200

    Update tasks for working with relational, logical and conditional operations

--- CppRevisionTasksProject/Tasks.h
+++ CppRevisionTasksProject/Tasks.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "Header.h"
 
-int task01(int, int);
-int task02(int, int);
-string task03(int, int, int);
-string task04(int, int, int);
-string task05(int, int, int);
+string task01(int, int, int, int);
+string task02(int, int, int, int);
+string task03(int, int, int, int);
+string task04(int, int, int, int);
+string task05(int, int, int, int);
 string task06(int, int, int, int);
--- CppRevisionTasksProject/Tests.h
+++ CppRevisionTasksProject/Tests.h
@@ -1,103 +1,175 @@
 #pragma once
 #include "Tasks.h"
 
+#define MIN_NUMBER 1
+#define MAX_NUMBER 8
+
 #define RIGHT "completed successfully. Well DONE!!!"
 #define WRONG "was not running successfully. Something was WRONG!"
 
 void testingTask01() {
+	bool result = true;
 
-	bool result = task01(10, 15) == 15
-		&& task01(40, 25) == 40
-		&& task01(7, 7) == 7;
+	for (int x = MIN_NUMBER; x <= MAX_NUMBER; x++)
+	{
+		for (int y = MIN_NUMBER; y < MAX_NUMBER; y++)
+		{
+			if (task01(x, y, x, y + 1) != "YES") {
+				result = false;
+				goto lable;
+			}
+		}
+	}
 
+	for (int x = MIN_NUMBER; x <= MAX_NUMBER; x++)
+	{
+		for (int y = MIN_NUMBER; y < MAX_NUMBER; y++)
+		{
+			int d1 = rand() % MAX_NUMBER;
+			int d2 = rand() % MAX_NUMBER;
+
+			if (task01(x, y, x + d1, y + d2) != "NO") {
+				result = false;
+				goto lable;
+			}
+		}
+	}
+
+lable:
 	cout << "Task 01 " << (result ? RIGHT : WRONG) << endl;
 }
 
 void testingTask02() {
-
-	bool result = task02(7, 6) == 1
-		&& task02(6, 7) == 2
-		&& task02(7, 7) == 0;
+	bool result = task02(4, 4, 5, 5) == "NO"
+		&& task02(4, 4, 3, 3) == "NO"
+		&& task02(4, 4, 3, 5) == "NO"
+		&& task02(4, 4, 5, 3) == "NO"
+		&& task02(4, 4, 7, 1) == "NO"
+		&& task02(4, 4, 1, 7) == "NO"
+		&& task02(4, 4, 1, 1) == "NO"
+		&& task02(4, 4, 7, 7) == "NO"
+		&& task02(4, 4, 4, 4) == "NO"
+		&& task02(4, 4, 4, 1) == "YES"
+		&& task02(4, 4, 4, 2) == "YES"
+		&& task02(4, 4, 4, 3) == "YES"
+		&& task02(4, 4, 4, 5) == "YES"
+		&& task02(4, 4, 4, 6) == "YES"
+		&& task02(4, 4, 4, 7) == "YES"
+		&& task02(4, 4, 4, 8) == "YES"
+		&& task02(4, 4, 1, 4) == "YES"
+		&& task02(4, 4, 2, 4) == "YES"
+		&& task02(4, 4, 3, 4) == "YES"
+		&& task02(4, 4, 5, 4) == "YES"
+		&& task02(4, 4, 6, 4) == "YES"
+		&& task02(4, 4, 7, 4) == "YES"
+		&& task02(4, 4, 8, 4) == "YES";
 
 	cout << "Task 02 " << (result ? RIGHT : WRONG) << endl;
 }
 
 void testingTask03() {
 
-	bool result = task03(1, 1, 1) == "YES"
-		&& task03(3, 4, 5) == "YES"
-		&& task03(5, 3, 4) == "YES"
-		&& task03(4, 5, 3) == "YES"
-		&& task03(1, 2, 3) == "NO"
-		&& task03(1, 1, 3) == "NO"
-		&& task03(1, 3, 1) == "NO"
-		&& task03(3, 1, 1) == "NO";
+	bool result = task03(5, 3, 5, 5) == "NO"
+		&& task03(5, 3, 3, 3) == "NO"
+		&& task03(5, 3, 5, 3) == "NO"
+		&& task03(5, 3, 3, 5) == "NO"
+		&& task03(5, 3, 7, 1) == "NO"
+		&& task03(5, 3, 1, 7) == "NO"
+		&& task03(5, 3, 3, 1) == "NO"
+		&& task03(5, 3, 7, 5) == "NO"
+		&& task03(5, 3, 7, 3) == "NO"
+		&& task03(5, 3, 5, 4) == "YES"
+		&& task03(5, 3, 5, 2) == "YES"
+		&& task03(5, 3, 6, 3) == "YES"
+		&& task03(5, 3, 4, 3) == "YES"
+		&& task03(5, 3, 4, 2) == "YES"
+		&& task03(5, 3, 4, 4) == "YES"
+		&& task03(5, 3, 6, 2) == "YES"
+		&& task03(5, 3, 6, 4) == "YES";
+
 
 	cout << "Task 03 " << (result ? RIGHT : WRONG) << endl;
 }
 
 void testingTask04() {
-
-	bool result = task04(4, 2, 6) == "YES"
-		&& task04(3, 4, 4) == "YES"
-		&& task04(5, 5, 10) == "YES"
-		&& task04(1, 1, 1) == "NO"
-		&& task04(7, 7, 7) == "YES" 
-		&& task04(3, 5, 1) == "NO"
-		&& task04(3, 5, 2) == "NO"
-		&& task04(3, 5, 3) == "YES"
-		&& task04(3, 5, 4) == "NO"
-		&& task04(3, 5, 5) == "YES"
-		&& task04(3, 5, 6) == "YES"
-		&& task04(3, 5, 7) == "NO"
-		&& task04(3, 5, 8) == "NO"
-		&& task04(3, 5, 9) == "YES"
-		&& task04(3, 5, 10) == "YES"
-		&& task04(3, 5, 11) == "NO"
-		&& task04(3, 5, 12) == "YES"
-		&& task04(3, 5, 13) == "NO"
-		&& task04(3, 5, 14) == "NO"
-		&& task04(3, 5, 15) == "NO"
-		&& task04(2, 10, 7) == "NO"
-		&& task04(9, 9, 3) == "NO"
-		&& task04(1, 8, 10) == "NO"
-		&& task04(1, 5, 6) == "NO"
-		&& task04(2, 4, 9) == "NO";
+	bool result = task04(6, 4, 5, 4) == "NO"
+		&& task04(6, 4, 6, 4) == "NO"
+		&& task04(6, 4, 7, 4) == "NO"
+		&& task04(6, 4, 6, 3) == "NO"
+		&& task04(6, 4, 6, 5) == "NO"
+		&& task04(6, 4, 8, 2) == "YES"
+		&& task04(6, 4, 7, 3) == "YES"
+		&& task04(6, 4, 4, 6) == "YES"
+		&& task04(6, 4, 3, 7) == "YES"
+		&& task04(6, 4, 2, 8) == "YES"
+		&& task04(6, 4, 3, 1) == "YES"
+		&& task04(6, 4, 4, 2) == "YES"
+		&& task04(6, 4, 5, 3) == "YES"
+		&& task04(6, 4, 7, 5) == "YES"
+		&& task04(6, 4, 8, 6) == "YES"
+		&& task04(6, 4, 5, 5) == "YES";
 
 	cout << "Task 04 " << (result ? RIGHT : WRONG) << endl;
 }
 
 void testingTask05() {
+	bool result = task05(6, 4, 5, 2) == "NO"
+		&& task05(6, 4, 6, 4) == "NO"
+		&& task05(6, 4, 2, 6) == "NO"
+		&& task05(6, 4, 1, 1) == "NO"
+		&& task05(6, 4, 8, 8) == "NO"
+		&& task05(6, 4, 3, 6) == "NO"
+		&& task05(6, 4, 4, 5) == "NO"
+		&& task05(6, 4, 5, 7) == "NO"
+		&& task05(6, 4, 8, 3) == "NO"
+		&& task05(6, 4, 8, 2) == "YES"
+		&& task05(6, 4, 7, 3) == "YES"
+		&& task05(6, 4, 5, 5) == "YES"
+		&& task05(6, 4, 4, 6) == "YES"
+		&& task05(6, 4, 3, 7) == "YES"
+		&& task05(6, 4, 2, 8) == "YES"
+		&& task05(6, 4, 3, 1) == "YES"
+		&& task05(6, 4, 4, 2) == "YES"
+		&& task05(6, 4, 5, 3) == "YES"
+		&& task05(6, 4, 7, 5) == "YES"
+		&& task05(6, 4, 8, 6) == "YES"
+		&& task05(6, 4, 5, 4) == "YES"
+		&& task05(6, 4, 4, 4) == "YES"
+		&& task05(6, 4, 3, 4) == "YES"
+		&& task05(6, 4, 2, 4) == "YES"
+		&& task05(6, 4, 1, 4) == "YES"
+		&& task05(6, 4, 7, 4) == "YES"
+		&& task05(6, 4, 8, 4) == "YES"
+		&& task05(6, 4, 6, 3) == "YES"
+		&& task05(6, 4, 6, 2) == "YES"
+		&& task05(6, 4, 6, 1) == "YES"
+		&& task05(6, 4, 6, 6) == "YES"
+		&& task05(6, 4, 6, 7) == "YES"
+		&& task05(6, 4, 6, 8) == "YES"
+		&& task05(6, 4, 6, 5) == "YES";
 
-	bool result = task05(4, 6, 5) == "NO"
-		&& task05(3, 5, 5) == "YES"
-		&& task05(1, 1, 1) == "YES"
-		&& task05(1, 1, 2) == "YES"
-		&& task05(1, 2, 2) == "YES"
-		&& task05(2, 1, 2) == "YES"
-		&& task05(2, 1, 1) == "NO"
-		&& task05(1, 2, 1) == "NO";
 
 	cout << "Task 05 " << (result ? RIGHT : WRONG) << endl;
 }
 
 void testingTask06() {
 
-	bool result = task06(4, 6, 5, 5) == "NO"
-		&& task06(3, 5, 5, 4) == "YES"
-		&& task06(1, 1, 1, 1) == "YES"
-		&& task06(2, 2, 2, 2) == "YES"
-		&& task06(1, 2, 2, 2) == "YES"
-		&& task06(2, 1, 2, 2) == "YES"
-		&& task06(1, 2, 1, 2) == "YES"
-		&& task06(1, 2, 2, 1) == "YES"
-		&& task06(2, 1, 1, 2) == "YES"
-		&& task06(2, 1, 2, 1) == "YES"
-		&& task06(3, 2, 2, 2) == "NO"
-		&& task06(2, 3, 2, 2) == "NO"
-		&& task06(3, 3, 2, 2) == "NO"
-		&& task06(3, 3, 3, 2) == "NO"
-		&& task06(3, 3, 2, 3) == "NO";
-		
+	bool result = task06(3, 6, 5, 3) == "NO"
+		&& task06(3, 6, 3, 6) == "NO"
+		&& task06(3, 6, 7, 5) == "NO"
+		&& task06(3, 6, 8, 6) == "NO"
+		&& task06(3, 6, 5, 4) == "NO"
+		&& task06(3, 6, 7, 4) == "NO"
+		&& task06(3, 6, 6, 3) == "NO"
+		&& task06(3, 6, 6, 5) == "NO"
+		&& task06(3, 6, 1, 5) == "YES"
+		&& task06(3, 6, 2, 4) == "YES"
+		&& task06(3, 6, 4, 4) == "YES"
+		&& task06(3, 6, 5, 5) == "YES"
+		&& task06(3, 6, 5, 7) == "YES"
+		&& task06(3, 6, 4, 8) == "YES"
+		&& task06(3, 6, 2, 8) == "YES"
+		&& task06(3, 6, 1, 7) == "YES";
+
 	cout << "Task 06 " << (result ? RIGHT : WRONG) << endl;
 }
