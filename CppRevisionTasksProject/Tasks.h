d87c3e6a610a08380b459922ec838b05f888b3ef
Victor <ivanvikvik@gmail.com>
2022-06-01 17:18:06 +0300

    Update Tasks.h

--- CppRevisionTasksProject/Tasks.h
+++ CppRevisionTasksProject/Tasks.h
@@ -1,10 +1,10 @@
 ﻿#pragma once
 #include "Header.h"
 
-int task01(long);
-int task02(long);
-int task03(long);
+int task01(long long);
+int task02(long long);
+int task03(long long);
 int task04(int);
 int task05(int);
-int task06(long);
-int task07(long);
+int task06(long long);
+int task07(long long);
